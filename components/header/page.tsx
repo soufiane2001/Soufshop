00e6f9867abd81f2086b3cb3fcb429e398b9df61
soufiane2001 <sboutatss@gmail.com>
2024-03-19 00:59:11 +0100

    first commit

---
+++ components/header/page.tsx
@@ -0,0 +1,39 @@
+import { FaShoppingCart } from 'react-icons/fa';
+const Header = () => {
+    return (
+        <nav className="bg-gray-800 py-6 ">
+        <div className="container px-28 mx-auto flex justify-between items-center">
+          <div>
+            <a href="/" className="text-white text-lg font-semibold">Your Logo</a>
+          </div>
+          <div className="flex items-center">
+            
+            {/* Add more navigation links as needed */}
+            <button  style={{ position: 'relative' }} className="bg-white text-gray-800 rounded-full p-2">
+            <FaShoppingCart />
+            <span
+          style={{
+            position: 'absolute',
+            top: '-8px',
+            right: '-19px',
+            backgroundColor: 'red',
+            color: 'white',
+            borderRadius: '100%',
+            paddingBottom: '4px',
+            paddingTop:'4px',
+      
+            paddingRight:'10px', 
+            paddingLeft:'10px',   
+            fontSize: '14px',
+          }}
+        >
+          {0}
+        </span>
+            </button>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Header;
